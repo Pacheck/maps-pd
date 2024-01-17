a9e9534c1db8f8c905e8e4c82187e45fe0f0de37
Daniel Mesquita <danielmesquitta123@gmail.com>
2024-01-17 07:40:51 -0300

    chore: add __all__ to modules

--- maps/__init__.py
+++ maps/__init__.py
@@ -1,2 +1,4 @@
 from .vegetation import buildVegetationMap
 from .geomorphology import buildGeomorphologyMap
+
+__all__ = ['buildVegetationMap', 'buildGeomorphologyMap']
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,5 @@
 from .geopandas import queryToGDF, asyncQueryToGDF, batchAsyncQueryToGDF
 from .ibge import getEstados, getCidades
+
+__all__ = ['queryToGDF', 'asyncQueryToGDF',
+           'batchAsyncQueryToGDF', 'getEstados', 'getCidades']
